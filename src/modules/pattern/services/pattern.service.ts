b8d991b41387976b053c429285316d04e1ff0e29
Mikita Yepishkin <myepishkin@exadel.com>
2023-09-22 03:07:27 +0200

    fix(backend): fixed promises

--- src/modules/pattern/services/pattern.service.ts
+++ src/modules/pattern/services/pattern.service.ts
@@ -12,10 +12,9 @@ export class PatternService {
   ) {}
 
   public async createPattern(createPatternDto: CreatePatternDto): Promise<PatternEntityWithId> {
-    //const { __v, ...newPattern } = new this.patternRepository(createPatternDto).save();
-    // const result = await (new this.patternRepository(createPatternDto).save());
-    // return Promise.resolve(newPattern);
-    return new this.patternRepository(createPatternDto).save();
+    const pattern = await (new this.patternRepository(createPatternDto).save());
+    const { __v, ...newPattern } = pattern;
+    return Promise.resolve(newPattern);
   }
 
   public async clone(pattern: PatternEntityWithId) {
